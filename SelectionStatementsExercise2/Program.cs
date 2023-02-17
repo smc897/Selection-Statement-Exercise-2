5cd88d1030e063417719c774f70d270cb2b712eb
ScottMC89 <scottmc592@gmail.com>
2023-02-17 15:40:33 -0600

    subjects.

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            var subject = Console.ReadLine();
+            switch (subject) {
+                case "Math": {
+                        Console.WriteLine("Math is your favorite subject.");
+                        break;
+                    }
+
+                case "Reading":
+                    {
+                        Console.WriteLine("Reading is your favorite subject.");
+                        break;
+                    }
+
+                case "English":
+                    {
+                        Console.WriteLine("English is your favorite subject.");
+                        break;
+                    }
+
+                case "Science":
+                    {
+                        Console.WriteLine("Science is your favorite subject.");
+                        break;
+                    }
+
+                case "History":
+                    {
+                        Console.WriteLine("History is your favorite subject.");
+                        break;
+                    }
+
+                case "Default":
+                    {
+                        Console.WriteLine("Nothing is your favorite subject.");
+                        break;
+                    }
+            }
         }
     }
 }
